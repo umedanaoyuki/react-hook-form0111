0ab84333217b7e403b038510fdd057da00f814b4
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-01-11 23:54:48 +0900

    ラベルの追加

--- src/App.js
+++ src/App.js
@@ -18,7 +18,43 @@ export default function FormBasic() {
     const onerror = err => console.log(err);
 
 return (
-    <p>テスト</p>
+    <form onSubmit={handleSubmit(onsubmit, onerror)} noValidate>
+        <div>
+            <label htmlFor="name">名前:</label><br/>
+            <input id="name" type="text" {...register('name', {
+                required: '名前は必須です。',
+                maxLength: {
+                    value: 20,
+                    message: '名前は20文字以内にしてください。'
+                }
+            })}　/>
+            <div>{errors.name?.message}</div>
+        </div>
+
+        <div>
+            <label htmlFor='gender'>性別：</label><br/>
+            <label>
+                <input type="radio" value="male"
+                    {...register('gender', {
+                        required: '性別は必須です。',
+                    })} />男性
+            </label>
+            <label>
+                <input type="radio" value="female"
+                       {...register('gender', {
+                           required: '性別は必須です。',
+                       })} />女性
+            </label>
+            <div>{errors.gender?.message}</div>
+        </div>
+
+        
+
+
+
+
+
+    </form>
 )
 
 
