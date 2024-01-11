822eaca62a94a1b1733283f948bf2a7da00c5885
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-01-11 23:27:15 +0900

    react-hook-form install

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hooks-form';
 
-function App() {
-  return (
-    <div className="App">
-      <p>テスト</p>
-    </div>
-  );
+export default function FormBasic() {
+
+    const defaultValue = {
+        name: '山田太郎',
+        email: 'admin@example.com',
+        gender: 'male',
+        memo: ''
+    };
+
+    const { register, handleSubmit, formState: { errors }}  = useForm({
+        defaultValue
+    });
+
+    const onsubmit = data => console.log(data);
+    const onerror = err => console.log(err);
+
+
 }
-
-export default App;
